a9afd6837896989d8cad206d677a114b9433d90a
breechoracion <oracionbre@gmail.com>
2020-11-12 02:19:01 +0800

    added cors

--- src/index.js
+++ src/index.js
@@ -20,7 +20,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const router = express.Router();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://13.229.71.34:5000",
+  })
+);
 app.use(express.static(__dirname + "/tmp"));
 app.use(router);
 router.use(express.json());
